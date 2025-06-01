03bb15f6a339adb58e8cc4f8da48f19c171d7af3
saemsplays <saemsplays@gmail.com>
2025-06-01 06:29:23 +0300

    Update App.tsx
    added self closing Analytics tag

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-      </Analytics>
+      <Analytics />
     </TooltipProvider>
   </QueryClientProvider>
 );
