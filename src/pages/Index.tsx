eb02c5f109fb354e89df518103665a2b67201d43
saemsplays <saemsplays@gmail.com>
2025-06-01 04:17:01 +0300

    Update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -89,7 +89,7 @@ Citizen of Kenya`);
   const incrementPageView = async () => {
     try {
       const { error } = await supabase.rpc('increment_user_action', {
-        action_type_param: 'page_view'
+        action_type_param: 'view'
       });
       
       if (error) {
