3a7829e994c54d964806313683275310115477d8
saemsplays <saemsplays@gmail.com>
2025-06-03 04:21:32 +0300

    Update BillsSidebar.tsx
    TEST - height of FAB

--- src/components/BillsSidebar.tsx
+++ src/components/BillsSidebar.tsx
@@ -952,7 +952,7 @@ Sincerely,
 
 // Floating Action Button for Mobile
 const MobileFAB: React.FC<{ onClick: () => void }> = ({ onClick }) => (
-  <div className="fixed bottom-18 left-4 z-50 lg:hidden">
+  <div className="fixed bottom-20 left-4 z-50 lg:hidden">
     <Button
       className="rounded-full w-14 h-14 shadow-lg"
       onClick={onClick}
