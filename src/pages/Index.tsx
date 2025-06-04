dfa3a0ecaf31aaceb221611502c02731707f58f9
saemsplays <saemsplays@gmail.com>
2025-06-05 01:31:07 +0300

    Update Index.tsx
    TEST

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -298,7 +298,7 @@ Citizen of Kenya`);
   ];
 
   return (
-    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300">
+    <div className="min-h-screen bg-white dark:bg-gray-900 transition-colors duration-300 pb-20">
       <SimpleTour
         isActive={showTour}
         onComplete={handleTourComplete}
