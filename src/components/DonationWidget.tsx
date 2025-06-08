4ae7a97d248b17b7b1c4bec2c83313d1b386ce98
saemsplays <saemsplays@gmail.com>
2025-06-08 23:12:15 +0300

    Update DonationWidget.tsx

--- src/components/DonationWidget.tsx
+++ src/components/DonationWidget.tsx
@@ -32,7 +32,7 @@ interface DonationWidgetProps {
   offsetY?: number;
 }
 
-const DonationWidget: React.FC<DonationWidgetProps> = ({ onTimedOut, isVisible: controlledVisibility, offsetY = 128 }) => {
+const DonationWidget: React.FC<DonationWidgetProps> = ({ onTimedOut, isVisible: controlledVisibility, offsetY = 135 }) => {
   const [isVisible, setIsVisible] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
   const [isHovering, setIsHovering] = useState(false);
