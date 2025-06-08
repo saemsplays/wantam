c1ed778263126385ed4cff8019e08488f7bbe5c2
saemsplays <saemsplays@gmail.com>
2025-06-08 09:53:39 +0300

    Update ScrollProgressTracker.tsx

--- src/components/ScrollProgressTracker.tsx
+++ src/components/ScrollProgressTracker.tsx
@@ -40,7 +40,7 @@ export const ScrollProgressTracker: React.FC<ScrollProgressTrackerProps> = ({
   };
 
   return (
-    <div className={`fixed left-8 top-1/2 transform -translate-y-1/2 z-50 hidden md:flex flex-col items-centertransition-all duration-300 ease-in-out transform ${
+    <div className={`fixed left-8 top-1/2 transform -translate-y-1/2 z-50 hidden md:flex flex-col items-center transition-all duration-300 ease-in-out transform ${
         isVisible
         ? 'opacity-100 translate-y-0 pointer-events-auto'
         : 'opacity-0 translate-y-4 pointer-events-none'
