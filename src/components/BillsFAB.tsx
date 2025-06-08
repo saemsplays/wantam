e076e85070880fe3c4b4ec9d89f4d78c85b89b28
saemsplays <saemsplays@gmail.com>
2025-06-08 08:59:44 +0300

    Update BillsFAB.tsx

--- src/components/BillsFAB.tsx
+++ src/components/BillsFAB.tsx
@@ -28,11 +28,11 @@ const BillsFAB = () => {
   };
 
   return (
-    <div className={"fixed left-4 bottom-36 z-30 transition-all duration-300 ease-in-out transform ${
+    <div className="fixed left-4 bottom-36 z-30 transition-all duration-300 ease-in-out transform ${
     isVisible
       ? 'opacity-100 translate-y-0 pointer-events-auto'
       : 'opacity-0 translate-y-4 pointer-events-none'
-  }`}">
+  }`">
       <AnimatePresence>
         {isExpanded && (
           <>
